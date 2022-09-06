d8e5b38c0bbc9e4b73c278b2ea7a715dd5f6772d
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-09-06 17:06:03 -0300

    realiza o exercicio 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LoadingComponent from '../pages/Components/LoadingComponent';
 import { getUser } from '../services/userAPI';
 
@@ -26,11 +27,14 @@ class Header extends React.Component {
     }
     return (
       <header data-testid="header-component">
-        <p data-testid="header-user-name">
+        <h1 data-testid="header-user-name">
           Olá
           {' '}
           {name}
-        </p>
+        </h1>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
