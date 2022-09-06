e2d8a3015ce7d687c0d7c1520b65a05c39a15dd2
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-09-06 13:54:18 -0300

    realiza o exercicio 2

---
+++ src/pages/Components/LoadingComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoadingComponent extends React.Component {
+  render() {
+    return (
+        <p>Carregando...</p>
+    );
+  }
+}
+
+export default LoadingComponent;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import {createUser} from '../services/userAPI';
+import LoadingComponent from './Components/LoadingComponent';
 
 class Login extends React.Component {
+  constructor() {
+    super()
+    this.handleChange = this.handleChange.bind(this);
+    this.createUserLoading = this.createUserLoading.bind(this);
+    this.state = {
+      isButtonDisabled: true,
+      name: '',
+      loading: false,
+    }
+  }
+
+  handleChange = (event) => {
+    if(event.target.value.length >= 3) {
+      this.setState({isButtonDisabled: false,
+        name: event.target.value,
+      })
+    } else {
+      this.setState({
+        isButtonDisabled: true,
+        name: event.target.value,
+      })
+    }
+  }
+  
+  createUserLoading = async (name) => {
+  const {history} = this.props
+    this.setState({loading: true})
+    await createUser({name: name});
+    history.push('/search')
+  }
+
+  
   render() {
-    return (
-      <div data-testid="page-login" />
-    );
+    const {isButtonDisabled, name, loading} = this.state;
+
+    if (loading === true){
+      return <LoadingComponent/>
+    } else {
+      return (
+        <div data-testid="page-login">
+          <label htmlFor='input-name'>
+            Login
+            <input type='text' data-testid="login-name-input" 
+            name='input-name' onChange={this.handleChange}/>
+            <button type='submit' data-testid="login-submit-button" 
+            disabled={isButtonDisabled} 
+            onClick={()=>{
+              this.createUserLoading(name);
+              }
+            }>
+              Entrar</button>
+          
+          </label>
+        </div>
+      );
+    }
   }
 }
 
