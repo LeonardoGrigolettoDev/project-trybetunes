6b74ee82dcb9e0623120f4bd16a9bccc4c655f56
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-09-06 14:53:46 -0300

    realiza o exercicio 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,65 +1,76 @@
 import React from 'react';
-import {createUser} from '../services/userAPI';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 import LoadingComponent from './Components/LoadingComponent';
 
 class Login extends React.Component {
   constructor() {
-    super()
+    super();
     this.handleChange = this.handleChange.bind(this);
     this.createUserLoading = this.createUserLoading.bind(this);
     this.state = {
       isButtonDisabled: true,
       name: '',
       loading: false,
-    }
+    };
   }
 
   handleChange = (event) => {
-    if(event.target.value.length >= 3) {
-      this.setState({isButtonDisabled: false,
+    const numberOfLetters = 3;
+    if (event.target.value.length >= numberOfLetters) {
+      this.setState({ isButtonDisabled: false,
         name: event.target.value,
-      })
+      });
     } else {
       this.setState({
         isButtonDisabled: true,
         name: event.target.value,
-      })
+      });
     }
-  }
-  
+  };
+
   createUserLoading = async (name) => {
-  const {history} = this.props
-    this.setState({loading: true})
-    await createUser({name: name});
-    history.push('/search')
-  }
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name });
+    history.push('/search');
+  };
 
-  
   render() {
-    const {isButtonDisabled, name, loading} = this.state;
-
-    if (loading === true){
-      return <LoadingComponent/>
-    } else {
-      return (
-        <div data-testid="page-login">
-          <label htmlFor='input-name'>
-            Login
-            <input type='text' data-testid="login-name-input" 
-            name='input-name' onChange={this.handleChange}/>
-            <button type='submit' data-testid="login-submit-button" 
-            disabled={isButtonDisabled} 
-            onClick={()=>{
-              this.createUserLoading(name);
-              }
-            }>
-              Entrar</button>
-          
-          </label>
-        </div>
-      );
+    const { isButtonDisabled, name, loading } = this.state;
+    console.log(this.props);
+    if (loading === true) {
+      return <LoadingComponent />;
     }
+    return (
+      <div data-testid="page-login">
+        <label htmlFor="input-name">
+          Login
+          <input
+            type="text"
+            data-testid="login-name-input"
+            name="input-name"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+            onClick={ () => {
+              this.createUserLoading(name);
+            } }
+          >
+            Entrar
+          </button>
+
+        </label>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
 
 export default Login;
