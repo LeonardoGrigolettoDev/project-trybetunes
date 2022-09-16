af4f77695aecd29090da99b4579ce7881f9c690b
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-09-16 16:13:10 -0300

    o teste bugou (?)

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import LoadingComponent from './Components/LoadingComponent';
 
 class ProfileEdit extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      // userInfos: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getUserInfos();
+  }
+
+  getUserInfos = async () => {
+    // this.setState({ loading: true });
+    // const userInfos = await getUser();
+    // this.setState({ loading: false, userInfos });
+  };
+
   render() {
+    console.log(getUser());
+    const { loading } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        {loading === true ? <LoadingComponent />
+          : <div />}
       </div>
     );
   }
