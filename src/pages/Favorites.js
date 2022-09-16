bcd37da4f35031a4653587009e81486421ed03e1
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-09-16 15:40:56 -0300

    corrige o exercicio

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import Header from '../components/Header';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
-import LoadingComponent from './Components/LoadingComponent';
-import MusicCard from './Components/MusicCard';
+// import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+// import LoadingComponent from './Components/LoadingComponent';
+// import MusicCard from './Components/MusicCard';
 
 class Favorites extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      favoriteSongs: [],
-      loading: false,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   // this.state = {
+  //   //   favoriteSongs: [],
+  //   //   loading: false,
+  //   // };
+  // }
 
   componentDidMount() {
-    this.setState({ loading: true });
-    let favorites = '';
-    const meAjuda = async () => { favorites = await getFavoriteSongs(); };
-    meAjuda();
-    this.setState({ favoriteSongs: favorites });
-    this.setState({ loading: false });
+    // this.setState({ loading: true });
+    // let favorites = '';
+    // const meAjuda = async () => { favorites = await getFavoriteSongs(); };
+    // meAjuda();
+    // this.setState({ favoriteSongs: favorites });
+    // this.setState({ loading: false });
   }
 
   render() {
-    const { favoriteSongs, loading } = this.state;
+    // const { favoriteSongs, loading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        {
+        {/* {
           loading === true
             ? <LoadingComponent /> : (<MusicCard reqAPI={ favoriteSongs } />)
-        }
+        } */}
 
       </div>
     );
