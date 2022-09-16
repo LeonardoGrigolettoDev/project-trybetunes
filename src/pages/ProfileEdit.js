c2e0099dd0f758ce7b3633e9b9dffc1916eb71a2
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-09-16 18:18:37 -0300

    tentativa do requisito 14

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-import { getUser } from '../services/userAPI';
+import { getUser, updateUser } from '../services/userAPI';
 import LoadingComponent from './Components/LoadingComponent';
 
 class ProfileEdit extends React.Component {
@@ -8,7 +9,11 @@ class ProfileEdit extends React.Component {
     super();
     this.state = {
       loading: false,
-      // userInfos: [],
+      newName: '',
+      newEmail: '',
+      newDescription: '',
+      newImg: '',
+      disabledButton: true,
     };
   }
 
@@ -17,19 +22,90 @@ class ProfileEdit extends React.Component {
   }
 
   getUserInfos = async () => {
-    // this.setState({ loading: true });
-    // const userInfos = await getUser();
-    // this.setState({ loading: false, userInfos });
+    this.setState({ loading: true });
+    const userInfos = await getUser();
+    this.setState({ loading: false });
+    this.setState({
+      newName: userInfos.name,
+      newEmail: userInfos.email,
+      newDescription: userInfos.description,
+      newImg: userInfos.image,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+    this.isSaveButtonDisabled();
+  };
+
+  handleClick = () => {
+    const { newName, newEmail, newDescription, newImg } = this.state;
+    updateUser({
+      name: newName,
+      email: newEmail,
+      image: newImg,
+      description: newDescription,
+    });
+  };
+
+  isSaveButtonDisabled = () => {
+    const { newName, newEmail, newDescription } = this.state;
+    if (newName.length > 0 && newEmail.length > 0
+      && newDescription.length > 0) {
+      this.setState({ disabledButton: false });
+    } else {
+      this.setState({ disabledButton: true });
+    }
   };
 
   render() {
-    console.log(getUser());
-    const { loading } = this.state;
+    const { loading, newName, newEmail,
+      newDescription, newImg, disabledButton } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
         {loading === true ? <LoadingComponent />
-          : <div />}
+          : (
+            <div>
+              <input
+                type="text"
+                data-testid="edit-input-image"
+                id="newImg"
+                onChange={ this.handleChange }
+              />
+              <img src={ newImg } alt="user" />
+              <input
+                type="text"
+                data-testid="edit-input-name"
+                id="newName"
+                value={ newName }
+                onChange={ this.handleChange }
+              />
+              <input
+                type="email"
+                data-testid="edit-input-email"
+                id="newEmail"
+                value={ newEmail }
+                onChange={ this.handleChange }
+              />
+              <input
+                type="text"
+                id="newDescription"
+                data-testid="edit-input-description"
+                value={ newDescription }
+                onChange={ this.handleChange }
+              />
+              <Link to="/profile">
+                <button
+                  data-testid="edit-button-save"
+                  type="submit"
+                  onClick={ this.handleClick }
+                  disabled={ disabledButton }
+                >
+                  Salvar
+                </button>
+              </Link>
+            </div>)}
       </div>
     );
   }
